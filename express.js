7fe4b1a0edbf93f6ee3d69c39b1e0845d8dd8528
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-08-12 22:38:07 +0530

    dynamic hoisting

--- express.js
+++ express.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.send(`home page ${req.query.name}`);
+    res.send('listening on port 8000');
 })
 
-app.listen(8000,()=>console.log('listening on port 8000'));
+app.listen(8000,()=>console.log('listening on port 8000 successfully'));
+
+app.get('/about',(req,res)=>{
+    res.send('about page');
+})
